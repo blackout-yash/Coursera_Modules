8ac6af3cbdf5cca2d9be65c41634ba3a53a258ed
Yash Kumar <yashkumarjmp2002@gmail.com>
2021-09-27 18:03:22 +0530

    Module 4

--- Module 4/script.js
+++ Module 4/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yash", "Jack", "Jonathan", "James", "Phoenix", "Frankie", "Liam", "Patrick", "Leo", "Jacob"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
